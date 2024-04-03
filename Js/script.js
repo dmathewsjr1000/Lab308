d5464a9120e0f8940295bbab22e41d5689b37d70
Dennis C. Mathews Jr. <dmathewsjr07106@gmail.com>
2024-04-03 10:19:04 -0400

    updated files

--- Js/script.js
+++ Js/script.js
@@ -43,3 +43,58 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+
+  const isDiv5 = (n1 % 5) && (n2 % 5) && (n3 % 5) && (n4 % 5) == 0;
+  console.log('These numbers are multiple of 5');
+
+  const isGreat = console.log((n1 > n4));
+  console.log('The first number is greater than last number');
+
+//   Subtracting Numbers
+  const isSub = n1 - n2;
+  console.log('The subtracted number ' + isSub);
+//   Multiplying Numbers
+  const isMul = isSub * n3;
+  console.log('The multiplied number ' + isMul);
+//   Getting the Remainder
+  const isRemain = isMul % n4;
+  console.log('The remainder number ' + isRemain);
+
+//   Testing that 25 is the Max number
+const isMax25 = console.log( n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25);
+console.log('25 is still Max number');
+
+
+const totalDistant = 1500;
+const totalBudget = 175;
+const avgGas = 3;
+const planAgas = totalDistant / 30;
+const planBgas = totalDistant / 28;
+const planCgas = totalDistant / 23;
+const planAbudget = planAgas * avgGas;
+const planBbudget = planBgas * avgGas;
+const planCbudget = planCgas * avgGas;
+const totalTime = totalDistant / 60;
+const budgetAsavings = totalBudget - planAbudget;
+const budgetBsavings = totalBudget - planBbudget;
+
+console.log('How many gallons of fuel will you need for the entire trip? ' + 
+'\nIf Plan A then the gallon is: ' + planAgas + 
+'\nIf Plan B then the gallon is: ' + planBgas + 
+'\nIf Plan C then the gallon is: ' + planCgas);
+
+console.log('Will your budget be enough to cover the fuel expense? ' + 
+'\nThe budget is: ' + totalBudget + 
+'\nIf Plan A then the budget is: ' + planAbudget +
+'\nIf Plan B then the budget is: ' + planBbudget +
+'\nIf Plan C then the budget is: ' + planCbudget +
+'\nBase on budget two out of three plans will be enough.');
+
+console.log('How long will the trip take in hours? ' + 
+'\nThe amount of time needed to make full trip will take ' + totalTime + ' hours.');
+
+console.log('After comparing all 3 of the miles per hours traveled from 75, 60, and 55: ' + 
+'\nThe 75mph way will not be a option because it out budget.' + 
+'\nThe 60mph way will be a option because it is budget and give you a savings of: ' + budgetAsavings +
+'\nThe 55mph way will be a option because it is budget and give you a savings of: ' + budgetBsavings);
